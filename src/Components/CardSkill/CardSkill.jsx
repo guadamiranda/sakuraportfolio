ef41ebe072c28d4e3a963a9a889486557ed436a6
Guada <guadamiranda@justiciacordoba.gob.ar>
2023-03-03 14:01:33 -0300

    Doing things

--- src/Components/CardSkill/CardSkill.jsx
+++ src/Components/CardSkill/CardSkill.jsx
@@ -1,26 +1,37 @@
-import '../../Styles/cardskills.scss'
+import '../../Styles/cardskills.scss';
 
-import { ImHtmlFive } from 'react-icons/im';
-import { VscCircleFilled } from 'react-icons/vsc'
+import { ImHtmlFive, ImCss3 } from 'react-icons/im';
+import { DiJavascript, DiMongodb } from 'react-icons/di';
+import { TbBrandAmongUs } from 'react-icons/tb';
+import { FaNodeJs, FaReact } from 'react-icons/fa';
+import { VscCircleFilled } from 'react-icons/vsc';
 
 export const CardSkill = () => {
+    const skills = [{name:'HTML', icon:<ImHtmlFive className='card-skill__icon-border'/>, level:'Intermediate'},
+                    {name:'CSS', icon:<ImCss3 className='card-skill__icon-border'/>, level:'Intermediate'},
+                    {name:'JavaScript', icon:<DiJavascript className='card-skill__icon-border'/>, level:'Intermediate'},
+                    {name:'React', icon:<FaReact className='card-skill__icon-border'/>, level:'Intermediate'},
+                    {name:'MongoDB', icon:<DiMongodb className='card-skill__icon-border'/>, level:'Basic'},
+                    {name:'Node', icon:<FaNodeJs className='card-skill__icon-border'/>, level:'Basic'}]
+
     return(
         <div className='card-skill'>
             <div className='card-skill__container'>
-                <div className='card-skill__icon-information-container'>
+                {skills.map((skill) => 
+                    <div className='card-skill__icon-information-container'>
                     <div className='card-skill__container-icons'>
                         <VscCircleFilled className='card-skill__icon'/>
                         <VscCircleFilled className='card-skill__icon'/>
-                        <ImHtmlFive className='card-skill__icon-border'/>
+                        {skill.icon}
                         <VscCircleFilled className='card-skill__icon'/>
                         <VscCircleFilled className='card-skill__icon'/>
                     </div>
                     
                     <div className='card-skill__information'>
-                        <span><b>HTML</b></span>
-                        <span>Intermediate</span>
+                        <span><b>{skill.name}</b></span>
+                        <span>{skill.level}</span>
                     </div>
-                </div>  
+                </div>)}
             </div>
         </div>
     )
